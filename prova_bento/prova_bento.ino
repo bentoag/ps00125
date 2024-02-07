58a27739b4d453eae4571106a4564ca8169e52e4
bentoag <profbentoag@gmail.com>
2024-02-07 09:41:28 -0300

    questao1_geral_code

--- prova_bento/prova_bento.ino
+++ prova_bento/prova_bento.ino
@@ -4,10 +4,11 @@
 #define tf2_delay pdMS_TO_TICKS(2000) //2Hz
 
 float temperaturaCelsius = 10.0;
+int i, j;
 
 //declaracao da tarefa
-void tf1(void *pvParameters)
-void tf2(void *pvParameters)
+void tf1(void *pvParameters);
+void tf2(void *pvParameters);
 
 void setup() {
   //inicializacao da serial
@@ -18,5 +19,32 @@ void setup() {
 }
 
 void loop() {
+  vTaskDelete(NULL);
+}
 
+void tf1(void *pvParameters){
+  (void) pvParameters;
+
+  for(i=0; i<100; i++){
+     temperaturaCelsius +=1.5; //incremento de 1.5 na temp em celsius
+    //imprimindo o resultado
+    Serial.print("Temperatura em ºC Celsius: ");
+    Serial.print(temperaturaCelsius);
+    vTaskDelay(tf1_delay);
+    }
+  
+ 
+}
+
+void tf2(void *pvParameters){
+  (void) pvParameters;
+  for(j=0; j<100; j++){
+    float temperaturaFahrenheit = ((temperaturaCelsius*(9.0/5.0))+ 32.0); //conversao ºC para ºF
+    //imprimindo o resultado
+    Serial.print("Temperatura em ºC Fahrenheit: ");
+    Serial.print(temperaturaFahrenheit);
+    vTaskDelay(tf2_delay);
+    
+  }
+  
 }
